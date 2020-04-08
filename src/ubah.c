3a5cdb4f671749fa1f6f27b37952b146efb51533
Reckordp <reckordp@gmail.com>
2020-04-08 22:50:54 +0700

    Menerapkan pecah pada `ubah.c`

--- src/ubah.c
+++ src/ubah.c
@@ -1,31 +1,37 @@
-#include <alay/ubah.h>
+#include <alay.h>
+#include <alay/pecah.h>
 
-void merubah_kalimat(char *kalimat, size_t ukuran, rangka_perubahan *ev, size_t u_ev) {
-	size_t uk_ev;
-	char *asal;
-	bool diganti;
+void ubah_teks(bagian_kalimat *bk, rangka_perubahan *rp) {
+	size_t jumlah_ketemu, urutan;
+	char **ketemu, saat_ini;
 
-	while (ukuran > 0) {
-		uk_ev = u_ev;
-		diganti = false;
+	urutan = strlen(rp->asal);
+	while(urutan) {
+		saat_ini = rp->asal[urutan - 1];
+		jumlah_ketemu = cari_pada_kalimat(saat_ini, bk, &ketemu);
 
-		while(uk_ev > 0) {
-			asal = ev[u_ev - uk_ev].asal;
-
-			while(*asal != '\0') {
-				if (*asal == *kalimat) {
-					diganti = true;
-					*kalimat = ev[u_ev - uk_ev].jadi;
-					break;
-				}
-				asal++;
+		while(jumlah_ketemu) {
+			if (*ketemu[jumlah_ketemu] == saat_ini) {
+				*ketemu[jumlah_ketemu] = rp->jadi;
 			}
-
-			if (diganti) break;
-			uk_ev--;
+			jumlah_ketemu--;
 		}
 
-		kalimat++;
-		ukuran--;
+		urutan--;
+	}
+}
+
+void merubah_kalimat(char *kalimat, size_t ukuran, rangka_perubahan *ev, size_t u_ev) {
+	size_t urutan;
+	bagian_kalimat *bk;
+
+	urutan = 0;
+	bk = malloc(sizeof(bagian_kalimat));
+	memset(bk, 0, sizeof(bagian_kalimat));
+	pecah_kalimat(bk, kalimat);
+
+	while (u_ev - urutan) {
+		ubah_teks(bk, rangka_perubahan + urutan);
+		urutan++;
 	}
 }
