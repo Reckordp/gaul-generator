95ecd092183cf145da9ba6fba1d752180c029354
Reckordp <reckordp@gmail.com>
2020-04-08 17:38:55 +0700

    Kerangka perubahan dari file

---
+++ src/alay.c
@@ -0,0 +1,192 @@
+#include <stdio.h>
+#include <string.h>
+#include <io.h>
+
+#define MAX_PENGUBAH (256)
+#define UKURAN_KALIMAT (2048)
+#define true (1)
+#define false (0)
+
+const char *daftar_file = \
+	"0: o, O\n" \
+	"1: i, I\n" \
+	"2: z, Z\n" \
+	"4: a, A\n" \
+	"5: s, S\n" \
+	"6: G\n" \
+	"7: t, d, T, D\n" \
+	"9: g";
+
+typedef unsigned char bool;
+
+typedef struct {
+	bool tanda;
+	char asal[16];
+	char jadi;
+} rangka_perubahan;
+
+typedef enum {
+	BARU, 
+	TITIK, 
+	PEMISAH, 
+	TUJUAN, 
+	SELESAI
+} jenis_penangkapan;
+
+typedef struct {
+	rangka_perubahan *tempat;
+	int penunjuk;
+	FILE *gudang;
+	jenis_penangkapan situasi;
+} penyerapan_informasi;
+
+bool olah_informasi(penyerapan_informasi *laporan) {
+#define L(badan) (laporan->badan)
+#define LT(badan) (L(tempat)->badan)
+#define SEL_JA \
+	do { \
+		L(situasi) = SELESAI; \
+		return true; \
+	} while(0)
+
+#define GANTI_GARIS \
+	do { \
+		L(situasi) = BARU; \
+		return true; \
+	} while(0)
+
+#define AMBIL_BARANG if (fread(&bacaan, sizeof(char), 1, L(gudang)) == 0) SEL_JA
+#define APA_GANTI_GARIS if (((int)bacaan) == 10) GANTI_GARIS
+#define BATASAN_ALPHA(c) (((c >= 97) && (c <= 122)) || ((c >= 65) && (c <= 90)))
+#define BATASAN_ANGKA(c) ((c >= 48) && (c <= 57))
+#define BATASAN (BATASAN_ALPHA((int)bacaan) || BATASAN_ANGKA((int)bacaan))
+
+	char bacaan;
+	switch(laporan->situasi) {
+		case BARU:
+		LT(tanda) = true;
+		L(penunjuk) = 0;
+		L(situasi) = TITIK;
+		break;
+	
+		case TITIK:
+		AMBIL_BARANG;
+		if (!BATASAN) SEL_JA;
+		LT(jadi) = bacaan;
+		L(situasi) = PEMISAH;
+		break;
+	
+		case PEMISAH:
+		AMBIL_BARANG;
+		if (BATASAN) SEL_JA;
+		L(situasi) = TUJUAN;
+		break;
+
+		case TUJUAN:
+		AMBIL_BARANG;
+		APA_GANTI_GARIS;
+		if (bacaan == 32) AMBIL_BARANG;
+		if (!BATASAN) SEL_JA;
+		LT(asal)[L(penunjuk)] = bacaan;
+		L(penunjuk)++;
+		AMBIL_BARANG;
+		APA_GANTI_GARIS;
+		if (bacaan != 44) SEL_JA;
+		break;
+	}
+
+	return false;
+
+#undef BATASAN
+#undef BATASAN_ANGKA
+#undef BATASAN_ALPHA
+#undef APA_GANTI_GARIS
+#undef AMBIL_BARANG
+#undef GANTI_GARIS
+#undef SEL_JA
+#undef LT
+#undef L
+}
+
+size_t dapatkan_pengubah(rangka_perubahan *tempat, size_t ukuran) {
+	penyerapan_informasi laporan;
+	size_t uk_t;
+
+	if (access("daftar", F_OK) < 0) {
+		printf("File gak ada buat satu\n");
+		FILE *df = fopen("daftar", "w");
+		fwrite(daftar_file, sizeof(char), strlen(daftar_file), df);
+		fclose(df);
+	}
+
+	laporan.tempat = tempat;
+	laporan.penunjuk = 0;
+	laporan.gudang = fopen("daftar", "r");
+	laporan.situasi = BARU;
+	uk_t = ukuran;
+
+	while(uk_t > 0) {
+		if (olah_informasi(&laporan)) {
+			if (laporan.situasi == SELESAI) break;
+			laporan.tempat++;
+			uk_t--;
+		}
+	}
+
+	fclose(laporan.gudang);
+	return ukuran - uk_t;
+}
+
+void merubah_kalimat(char *kalimat, size_t ukuran, rangka_perubahan *ev, size_t u_ev) {
+	size_t uk_ev;
+	char *asal;
+	bool diganti;
+
+	while (ukuran > 0) {
+		uk_ev = u_ev;
+		diganti = false;
+
+		while(uk_ev > 0) {
+			asal = ev[u_ev - uk_ev].asal;
+
+			while(*asal != '\0') {
+				if (*asal == *kalimat) {
+					diganti = true;
+					*kalimat = ev[u_ev - uk_ev].jadi;
+					break;
+				}
+				asal++;
+			}
+
+			if (diganti) break;
+			uk_ev--;
+		}
+
+		kalimat++;
+		ukuran--;
+	}
+}
+
+int main() {
+	char asal[UKURAN_KALIMAT];
+	rangka_perubahan evolusi[MAX_PENGUBAH];
+	size_t ukuran;
+
+	memset(evolusi, 0, MAX_PENGUBAH * sizeof(rangka_perubahan));
+	ukuran = dapatkan_pengubah(evolusi, MAX_PENGUBAH);
+
+	printf("================================\n");
+	printf("	Gaul Generator ");
+	printf("\n\n");
+
+	while(true) {
+		printf("Kalimat : ");
+		gets(asal);
+		if (*asal == '\0') break;
+		merubah_kalimat(asal, strlen(asal), evolusi, ukuran);
+		printf("=> %s\n\n", asal);
+	}
+
+	printf("================================\n");
+    return 0;
+}
